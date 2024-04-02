e7e7e328f5824688ac7507ec688e89de1ed7ce06
Alex Sobolev <31108@mail.ru>
2024-04-02 12:50:48 +0300

    Setup alert controller

--- RealmApp/Extension + AlertController.swift
+++ RealmApp/Extension + AlertController.swift
@@ -12,4 +12,50 @@ extension UIAlertController {
         UIAlertController(title: title, message: message, preferredStyle: .alert)
     }
     
+    func action(with taskList: TaskList?, completion: @escaping(String) -> Void) {
+        let doneButton = taskList == nil ? "Save" : "Delete"
+        
+        let saveAction = UIAlertAction(title: doneButton, style: .default) { _ in
+            guard let newValue = self.textFields?.first?.text else { return }
+            guard !newValue.isEmpty else { return }
+            completion(newValue)
+        }
+        
+        let cancelAction = UIAlertAction(title: doneButton, style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField { textField in
+            textField.placeholder = "List Name"
+            textField.text = taskList?.name
+        }
+    }
+    
+    func action(with task: Task?, completion: @escaping (String, String) -> Void) {
+        let title = task == nil ? "Save" : "Update"
+        
+        let saveAction = UIAlertAction(title: title, style: .default) { _ in
+            guard let newTask = self.textFields?.first?.text else { return }
+            guard !newTask.isEmpty else { return }
+            
+            if let note = self.textFields?.first?.text, !note.isEmpty {
+                completion(newTask, note)
+            } else {
+                completion(newTask, "")
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField { textField in
+            textField.placeholder = "New Task"
+            textField.text = task?.name
+        }
+        addTextField { textField in
+            textField.placeholder = "Note"
+            textField.text = task?.note
+        }
+    }
 }
